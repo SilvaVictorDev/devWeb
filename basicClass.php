5101b50099df99368f345027caa1e5be100e9bba
SilvaVictorDev <silva_victor@id.uff.br>
2025-10-16 07:00:29 -0300

    novo arquivo

---
+++ basicClass.php
@@ -0,0 +1,24 @@
+<?php
+class Fruta{
+    var $nome;
+    var $tipo;
+
+    public function __construct(){ } 
+    
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+}
